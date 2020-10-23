2a4c020dc0995dc88c9a6143a264540562b27e42
wake3532 <70120646+wake3532@users.noreply.github.com>
2020-10-23 18:06:46 +0900

    Add files via upload

---
+++ BS-dmbot-master/dmsend.py
@@ -0,0 +1,37 @@
+
+# 봉순#6959 : MASS DM BOT SOURCE
+
+
+import discord
+import asyncio
+import datetime
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("봇이 정상적으로 실행되었습니다.")
+    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+#/dm {할말}로 전체DM 전송
+@client.event
+async def on_message(message):
+    if message.content.startswith('/dm'):
+        for i in message.guild.members:
+            if i.bot == True:
+                pass
+            else:
+                try:
+                    msg = message.content[4:]
+                    #메시지 관리권한이 있을시 사용가능
+                    if message.author.guild_permissions.manage_messages:
+                        embed = discord.Embed(color=0x1DDB16, timestamp=message.created_at)
+                        embed.add_field(name="★★제목★★", value=msg, inline=True)
+                        embed.set_footer(text="★맨 밑에 들어갈 내용★")
+                        await i.send(embed=embed)
+                except:
+                    pass
+
+
+client.run('★★봇토큰★★')
